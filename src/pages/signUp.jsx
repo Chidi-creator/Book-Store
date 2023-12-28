afbd7f73c8fbca84c784d217cb6c35deaf338921
unknown <tyler.igwe@gmail.com>
2023-12-28 10:12:55 +0100

    another one

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -3,89 +3,84 @@ import useSignup from "../Hooks/useSignup";
 import { useAuthContext } from "../Hooks/useAuthContext";
 import { Link } from "react-router-dom";
 
-
 const Signup = () => {
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const {user} = useAuthContext()
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user } = useAuthContext();
 
-const { signup, error, loading} = useSignup()
+  const { signup, error, loading } = useSignup();
 
-
-const handleClick = async (e) =>{
-
-    e.preventDefault()
-    await signup(username, email, password)
-    
-}
-useEffect(() => {
+  const handleClick = async (e) => {
+    e.preventDefault();
+    await signup(username, email, password);
+  };
+  useEffect(() => {
     console.log(user);
   }, [user]);
 
-
-    return ( <div>
-    
-         <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="bg-white p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
-        <form>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Username
-            </label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
-              placeholder="Enter your username"
-              value={username}
-              onChange={(e) =>setUsername(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email
-            </label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) =>setPassword(e.target.value) }
-            />
-          </div>
-          {error && <p className="text-red-500 mt-4 mb-2">{error}</p>}
-          {loading && <p className="text-red-500 mt-4 mb-2">loading...</p>}
-          <button
-            
-            className="bg-blue-500 text-white p-2 rounded-md w-full"
-            onClick={handleClick}
-            disabled={loading}
-          >
-            Sign Up
-          </button>
-        </form>
-         <p className="text-gray-600 text-sm mt-4">
-          Already have an account?{" "}
-          <Link to="/user/login" className="text-blue-500 hover:underline">
-          Log In
-          </Link>
-        </p>
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <div className="bg-white p-8 rounded shadow-md w-96">
+          <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
+          <form>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Username
+              </label>
+              <input
+                type="text"
+                className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Email
+              </label>
+              <input
+                type="text"
+                className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                type="password"
+                className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            {error && <p className="text-red-500 mt-4 mb-2">{error}</p>}
+            {loading && <p className="text-red-500 mt-4 mb-2">loading...</p>}
+            <button
+              className="bg-blue-500 text-white p-2 rounded-md w-full"
+              onClick={handleClick}
+              disabled={loading}
+            >
+              Sign Up
+            </button>
+          </form>
+          <p className="text-gray-600 text-sm mt-4">
+            Already have an account?{" "}
+            <Link to="/user/login" className="text-blue-500 hover:underline">
+              Log In
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
-    </div> );
-}
- 
+  );
+};
+
 export default Signup;
